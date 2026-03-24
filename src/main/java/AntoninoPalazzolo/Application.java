bf20583e87dc2016f090c35215ef9f07b38121bf
palax-99 <ninociao7@gmail.com>
2026-03-24 14:42:33 +0100

    test per vedere se fossi connesso col database

--- src/main/java/AntoninoPalazzolo/Application.java
+++ src/main/java/AntoninoPalazzolo/Application.java
@@ -1,8 +1,16 @@
 package AntoninoPalazzolo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("u1w3d2");
+    //mi sono assicurato di esseremi connesso al database
 
     public static void main(String[] args) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
         System.out.println("Hello World!");
     }
 }
